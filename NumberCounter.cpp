c9ca7628872cc696db7d43cd391282e4699093e0
konnov-007 <konnov-007@mail.ru>
2016-05-22 15:32:34 +0600

    Final version

--- NumberCounter.cpp
+++ NumberCounter.cpp
@@ -2,31 +2,35 @@
 #include <stdlib.h>
 
 int numberCounter(){
-int min=0, max=100000;
-int tmp1, tmp2;
-int countSum=0, count5,count3;
+	int min=0, max=100000;
+	int tmp1, tmp2;
+	int countSum=0, count5,count3;
 
 	FILE *output;
 	output = fopen ("NumbersCounter.txt", "w");
 	
-for(int i=min; i<max; i++){
-tmp1=i; count5=0; count3=0;
+	for(int i=min; i<max; i++)
+	{
+		tmp1=i; count5=0; count3=0;
 
-while(tmp1>0){
-tmp2=tmp1%10;
-if(tmp2==3) count3++;
-if(tmp2==5) count5++;
-tmp1/=10;
-}
-if(count3>=1&&count5==2)
-{
-	countSum++;
-	printf("\n%d, count = %d", i, countSum);
-	fprintf(output, "\n%d, count = %d", i, countSum);
-}
-}
-
-fclose(output);
+		while(tmp1>0)
+		{
+			tmp2=tmp1%10;
+			if(tmp2==3) 
+				count3++;
+			if(tmp2==5) 
+				count5++;
+			tmp1/=10;
+		}
+	
+		if(count3==1&&count5==2)
+		{
+			countSum++;
+			fprintf(output, "\n%d, count = %d", i, countSum);
+		}
+	}
+	fclose(output);
+	printf("Amount of two 5 numbers and one 3 number in range [0; 100000] is %d", countSum);
 }
 
 
